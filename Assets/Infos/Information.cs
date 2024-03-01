170a6d5a359c120c66b42bbdd500376f845932ab
Boki <79083667+EX3mKing@users.noreply.github.com>
2024-03-01 11:49:08 +0100

    opis za kod

--- Assets/Infos/Information.cs
+++ Assets/Infos/Information.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu(fileName = "I_", menuName = "ScriptableObjects/Information", order = 1)]
+
+// ova se klasa koristi za upis podataka o koracima prve pomoci
 public class Information : ScriptableObject
 {
     public List<string> title;
--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -5,6 +5,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+// ova klasa se koristi za kontroliranje aplikacije
 public class Controller : MonoBehaviour
 {
     private Vector2 touchStart;
@@ -26,37 +27,45 @@ public class Controller : MonoBehaviour
 
     private void Update()
     {
+        // Provjerava se ima li dodira na zaslonu i je li to početak dodira.
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
+            // Ako je to početak dodira, bilježi se početna pozicija dodira.
             touchStart = Input.GetTouch(0).position;
         }
         
+        // Provjerava se ima li dodira na zaslonu i je li to završetak dodira.
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
         {
+            // Ako je to završetak dodira, bilježi se krajnja pozicija dodira.
             touchEnd = Input.GetTouch(0).position;
 
-            float swipeLen = touchEnd.x - touchStart.x; 
+            // Izračunava se duljina poteza po x osi.
+            float swipeLen = touchEnd.x - touchStart.x;
 
+            // Provjerava se duljina poteza u odnosu na pragovnu vrijednost.
             if (swipeLen < -swipeThreshold)
             {
+                // Ako je duljina poteza manja od negativnog praga, izvršava se potez s lijeva na desno.
                 SwipeL2R();
             }
             else if (swipeLen > swipeThreshold)
             {
+                // Ako je duljina poteza veća od praga, izvršava se potez s desna na lijevo.
                 SwipeR2L();
             }
         }
     }
 
+    // provjerava listamo li u opisu koraka i onda prebacije na slijedeći sadržaj
     private void SwipeR2L()
     {
         Debug.Log("R2L");
         if (SwipingInDescription) { if (currentInfo > 0) SwipeDescription(-1); }
         else if (currentPage > 0) SwipePage(-1);
-        
-        
     }
     
+    // provjerava listamo li u opisu koraka i onda prebacije na slijedeći sadržaj
     private void SwipeL2R()
     {
         Debug.Log("L2R");
@@ -65,6 +74,7 @@ public class Controller : MonoBehaviour
         else if(currentPage + 1 < pages.Count) SwipePage(1);
     }
 
+    // provjerava listamo li u katalogu i onda prebacije na slijedeći sadržaj
     private void SwipePage(int amount)
     {
         currentPage+= amount;
@@ -72,6 +82,7 @@ public class Controller : MonoBehaviour
         pages[currentPage].SetActive(true);
     }
 
+    // zamijenjuje UI elemente unutar opisa koraka prilikom klizanja prsta
     private void SwipeDescription(int amount)
     {
         currentInfo += amount;
@@ -81,6 +92,7 @@ public class Controller : MonoBehaviour
         descriptionNumberText.text = currentInfo + 1 + "/" + curInfo.information.Count;
     }
     
+    // pritiskom na gumb učitava potrebne UI elemente za opis koraka
     public void LoadInfo(Information info)
     {
         GuideCanvas.SetActive(true);
@@ -93,6 +105,7 @@ public class Controller : MonoBehaviour
         descriptionNumberText.text = currentInfo + 1 + "/" + curInfo.information.Count;
     }
     
+    // nazad učitava glavni katalog 
     public void ToMainMenu()
     {
         GuideCanvas.SetActive(false);
